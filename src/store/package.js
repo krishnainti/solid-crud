dc35fc44fec3bb5d0ed076422d625b9029ca564b
krishnainti <ivkrish143007@gmail.com>
2023-01-08 23:33:44 +0530

    removed unsed code

--- src/store/package.js
+++ src/store/package.js
@@ -1,7 +1,5 @@
 import { createSignal } from "solid-js";
 
-const [packages, setPackages] = createSignal([
-  { name: "name", description: "description", id: 1 },
-]);
+const [packages, setPackages] = createSignal([]);
 
 export { packages, setPackages };
